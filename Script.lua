f2b6be350762642f4a68268d8ee0e95a9175bb3c
minigames-dev <iakia193105@gmail.com>
2026-02-07 20:16:52 -0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/minigames-dev/Script-brookhaven5/refs/heads/main/Script.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/minigames-dev/Script-brookhaven6/refs/heads/main/Script.lua"))()
